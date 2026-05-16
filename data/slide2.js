51a1b753a5e03e1da66c7789253b3723f7a43bb8
lcnhsk2 <lcnhsk2@gmail.com>
2026-05-16 09:48:54 +0700

    Add files via upload

---
+++ data/slide2.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{E2A8FB66-9598-4B42-AF85-1D5972F0F393}'];loadHandler&&loadHandler(1, '<div id="spr0_284b7d"><div id="spr1_284b7d" class="kern slide"><img id="img0_284b7d" src="data/img3.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_284b7d" class="kern slide"><div id="wo_1_0"></div></div></div>', '{"s":[]}');})();
